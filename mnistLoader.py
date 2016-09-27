a functioning gradient descent algo plus add a function which lets evaluate the results as well as get insight into the correct and incorrect examples that the network gets

mnistLoader.py
@@ -17,6 +17,7 @@ import gzip
 import numpy as np
 import scipy.misc as smp
 import random
+import utility
 
 
 
@@ -37,6 +38,7 @@ def readImages(file,numberOfPics):
     PICTURE_HEIGHT=28
     PICTURE_SIZE_IN_PIXELS=PICTURE_HEIGHT*PICTURE_WIDTH
     HEADER_SIZE_IN_BYTES=16
+    COLOR_MAX_INTENSITY=255
     #skip the header
     file.read(HEADER_SIZE_IN_BYTES)
     images=[]
@@ -45,10 +47,11 @@ def readImages(file,numberOfPics):
         picture=[ord(n) for n in currentPicInBytes] #This translates the string to numbers
         picInNp=np.asarray(picture,dtype=float)
         #If I want it in a viewable format: picInNp=picInNp.reshape(PICTURE_HEIGHT,PICTURE_WIDTH)
-        picInNp=picInNp/255
+        picInNp=picInNp/COLOR_MAX_INTENSITY
         images.append(picInNp)
     file.close()
-    images=np.asarray(images)
+    images=(np.asarray(images)).transpose()
+    
     return images
         
 
@@ -63,8 +66,12 @@ def readLabels(file,numberOfLabels):
     allLabelsInString=file.read(numberOfLabels)
     labels=[ord(n) for n in allLabelsInString]
     file.close()
-    labels=np.asarray(labels)
-    return labels
+    #And now after reading the numeric (0-9) value of the label,let's convert it into a vector.
+    vectorizedLabels=[]
+    for label in labels:
+        currentLabel=utility.vectorizeDigit(label)
+        vectorizedLabels.append(currentLabel)
+    return np.asarray(vectorizedLabels).transpose()
     
     
 """
@@ -90,7 +97,7 @@ def readData():
     #And now let's 'cut' the training data
     trainImages=trainImages[:50000,:]
     trainLabels=trainLabels[:50000]
-    
+    #training_data=[(image,label) for image,label in zip(trainImages,trainLabels)]
     training_data=(trainImages,trainLabels)
     validation_data=(validImages,validLabels)
     test_data=(testImages,testLabels)

network.py
@@ -56,7 +56,7 @@ class Network:
     TO DO: Add a parameter which will determine the activation function (and probably default it to sigmoid)
     """
     def feedForward(self,x):
-        a=x
+        a=x.reshape(len(x.flat),1)
         for (w,b) in zip(self.weights,self.biases):
             a=sigmoid(np.dot(w, a)+b)
         return a
@@ -66,6 +66,8 @@ class Network:
     #The output will be dC/dW and dC/dB, which are lists of the same size as the network weights and biases respectively.
     def backpropogation(self,x,y):
         #first I will feed forward the input x,calculating everything which needs to be calculated along its path
+        x=x.reshape(len(x.flat),1)
+        y=y.reshape(len(y.flat),1)
         activations=[x] #This will be a list of column vectors with the network's activations.
         Zs=[] #Likewise,Zs will be a list of the z's eg w*a+b before applying the activation function
         currentLayerOutput=x
@@ -106,7 +108,7 @@ class Network:
 
     """
     
-    def stochastic_grad(self,training_data,learningRate,epochs=50,batchSize=0):
+    def stochastic_grad(self,training_data,learningRate,epochs=50,batchSize=0,test_data=None):
     #So basically stochastic grad is the same as gradient descent when batch size is same as the size of our learning set.
     #That will be the default value then.
         if batchSize==0:
@@ -115,9 +117,57 @@ class Network:
         random.shuffle(training_data)
         training_examples=[]
         training_labels=[]
-        training_examples[:],training_labels[:]=zip(*training_data)     
-        utility.showPic(training_examples,training_labels)
+        training_examples[:],training_labels[:]=zip(*training_data)
+        
+        
+        
+        for epoch in range(epochs):
+            i=0.0
+            sum_dCdW=[np.zeros(weight.shape) for weight in self.weights]
+            sum_dCdB=[np.zeros(bias.shape) for bias in self.biases]
+            for (example,label) in zip(training_examples,training_labels):
+                i=i+1
+                #print 'summing for example {}'.format(i),
+                #utility.update_progress(i/len(training_examples))
+                
+                (dCdB,dCdW)=self.backpropogation(example,label)
+                sum_dCdW=[sum+dcdw for sum,dcdw in zip(sum_dCdW,dCdW)] #sum+=dcdw, unfortunately in python the plus operator on lists just concatenates them :)
+                sum_dCdB=[sum+dcdb for sum,dcdb in zip(sum_dCdB,dCdB)]
+                
+            self.biases=[currentBias-(float(learningRate)/batchSize)*theSum for currentBias,theSum in zip(self.biases,sum_dCdB)]
+            self.weights=[currentWeight-(float(learningRate)/batchSize)*theSum for currentWeight,theSum in zip(self.weights,sum_dCdW)]
+            if test_data:
+                percentsCorrect,_,_=self.evaluate(test_data)
+                print('after {} epochs I am right at {} percents of tests'.format(epoch,100*percentsCorrect))
+            
+            
+        
+        
+        
         return
+        
+    """
+    evaluate. 
+    Input- a tuple (x,y) where x and y are np arrays of examples and their labels
+    """
+    def evaluate(self, test_data):
+    
+        test_examples=test_data[0]
+        test_labels=test_data[1]
+        m=test_examples.shape[1]
+        correct=[]
+        wrong=[]
+        for i in range(m):
+            label=test_labels[:,i]
+            test_example=test_examples[:,i]
+            result=np.argmax(self.feedForward(test_example))
+            result=utility.vectorizeDigit(result)
+            if np.array_equal(label,result):
+                correct.append((test_example,label))
+            else:
+                wrong.append((test_example,label))
+            
+        return len(correct)/float(m),correct,wrong
     
     
         
@@ -167,10 +217,10 @@ def cost_prime(output,y):
     
     
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1.0/(1+np.exp(-x))
 
 def sigmoid_prime(x):
-    return sigmoid(x)*(1-sigmoid(x))
+    return sigmoid(x)*(1.0-sigmoid(x))
 
     
 
@@ -180,6 +230,20 @@ def calcWeights(activations,delta):
     for i in range(len(delta)):
         weights.append(np.dot(delta[i],activations[i].transpose()))
     return weights
+
+    
+    
+    
+def getSize(x):
+    size=0
+    for sth in x:
+        itsShape=sth.shape
+        if len(itsShape)==1:
+            size=size+itsShape[0]
+        else:
+            size=size+itsShape[0]*itsShape[1]
+            
+    return size
     
     
     

setit.py
@@ -9,16 +9,10 @@ net=network.Network([784,40,10])
 #First of all,load all the data...
 print('Loading the Data....')
 (training_data, validation_data, test_data)=loader.readData()
-training_examples=training_data[0].transpose()
-training_labels=[]
-for x in training_data[1]:
-    currentLabel=np.zeros(10)
-    currentLabel[x]=1
-    training_labels.append(currentLabel)
-    
-training_labels=(np.asarray(training_labels)).transpose()
+training_examples=training_data[0]
+training_labels=training_data[1]
 m=training_examples.shape[1]
 
 training_data=[(training_examples[:,i],training_labels[:,i]) for i in range(m)]
-
-net.stochastic_grad(training_data,learningRate=0.1,epochs=50,batchSize=0)
+    
+net.stochastic_grad(training_data,learningRate=3,epochs=10,batchSize=0,test_data=test_data)